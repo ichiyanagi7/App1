84195a9d1260d4d60ad32690e78786d89458871a
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-07-04 11:31:23 +0000

    [finish]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,9 +28,9 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog)
   end
 
-  def destory
+  def destroy
     blog=Blog.find(params[:id])
-    blog.destory
+    blog.destroy
     redirect_to blogs_path
   end
 
